a1bc4fa14a150d2807bdf9f561fac713dd536ca8
LD & Nick Perri <pair+ldinho.nperri6@devbootcamp.com>
2015-10-23 16:45:26 -0400

    Add bcrypt #password to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
 class User < ActiveRecord::Base
   has_many :rounds
+
+
+  def password
+    if password_digest.present?
+      @password ||= BCrypt::Password.new(password_digest)
+    end
+  end
+
+  def password=(new_password)
+    @password = BCrypt::Password.create(new_password)
+    self.password_digest = @password
+  end
+
 end
