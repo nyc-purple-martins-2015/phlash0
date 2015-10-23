c2753a43b27308e786ece214f0f9506c5827bf80
Rebecca Kleinberg <email": null>
2015-10-23 17:24:02 -0400

    added logout

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,7 +4,7 @@ end
 
 post '/login' do
   @user = User.find_by(email: params[:user][:email])
-  if @user.valid?
+  if !@user.nil?
     if @user.password == params[:password]
       session[:user_id] = @user.id
       redirect '/'
@@ -12,13 +12,14 @@ post '/login' do
       erb :'/user/login'
     end
   else
-    @errors = @user.errors.full_messages
-    erb :login
+    # @errors = @user.errors.full_messages
+    erb :'/user/login'
   end
 end
 
 get '/logout' do
-
+  session.clear
+  redirect '/'
 end
 
 get '/signup' do
