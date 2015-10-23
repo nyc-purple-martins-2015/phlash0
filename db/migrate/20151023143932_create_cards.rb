2d792732905fcd6a172cd0b34bb726b29f043ab6
LD & Nick Perri <pair+ldinho.nperri6@devbootcamp.com>
2015-10-23 14:57:31 -0400

    Add initial migrations and models

---
+++ db/migrate/20151023143932_create_cards.rb
@@ -0,0 +1,11 @@
+class CreateCards < ActiveRecord::Migration
+  def change
+    create_table :cards do |t|
+      t.string :question, null: false
+      t.string :answer, null: false
+      t.integer :deck_id
+
+      t.timestamps null: false
+    end
+  end
+end
