a33d9e1d77375e09ae99ca31cfd10d301b9cb391
Rebecca Kleinberg <email": null>
2015-10-23 17:46:30 -0400

    correct seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
-5.times do {
-  name: Faker::Name.name,
-  email: Faker::Internet.email,
-  password_digest: Faker::Internet.password(8)
-}
+5.times do
+  User.create!(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password_digest: Faker::Internet.password(8)
+    )
+end
