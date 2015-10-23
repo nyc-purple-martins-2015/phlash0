04e4ac24d7640232d1812c311a121057d77e3d37
LD & Nick Perri <pair+ldinho.nperri6@devbootcamp.com>
2015-10-23 16:21:27 -0400

    add user route skeleton

---
+++ app/controllers/deck_controller.rb
@@ -0,0 +1,6 @@
+get '/decks' do
+
+end
+
+
+
---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,22 @@
+get '/login' do
+
+end
+
+post '/login' do
+
+end
+
+get '/logout' do
+
+end
+
+get '/signup' do
+
+end
+
+
+post '/signup' do
+
+end
+
+
