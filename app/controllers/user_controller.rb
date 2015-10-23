38c102f41d43e0927ccad30c249bdf603f64553a
Rebecca Kleinberg <email": null>
2015-10-23 16:59:30 -0400

    work on login

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,9 +1,20 @@
 get '/login' do
-
+  erb :'/user/login'
 end
 
 post '/login' do
-
+  @user = User.find_by(email: params[:user][:email])
+  if @user.valid?
+    if @user.password == params[:password]
+      session[:user_id] = @user.id
+      redirect '/'
+    else
+      erb :'/user/login'
+    end
+  else
+    @errors = @user.errors.full_messages
+    erb :login
+  end
 end
 
 get '/logout' do
