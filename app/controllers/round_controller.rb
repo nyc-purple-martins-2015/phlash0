2246f810371136a1b6cf13857f61b5a942b4f8a7
Nick Perri <nperri6@gmail.com>
2015-10-24 18:13:37 -0400

    clean up page

--- app/controllers/round_controller.rb
+++ app/controllers/round_controller.rb
@@ -3,7 +3,7 @@ get '/rounds/:id' do
   # @guesses = Guess.where(round_id: session[:round_id]).where(is_correct: false)
   round = Round.find(session[:round_id])
   @guesses = round.guesses.where(is_correct: false)
-
+  @deck = round.deck
   @cards = []
   @guesses.each do |guess|
       @cards << Card.where(id: guess[:card_id])
