80537f9d9645585b29e75852936d9a16903da27c
Rebecca Kleinberg <email": null>
2015-10-24 13:20:10 -0400

    working on card_question.erb

--- app/controllers/deck_controller.rb
+++ app/controllers/deck_controller.rb
@@ -7,6 +7,19 @@ get '/decks/:deck_id' do
 
 end
 
+get '/decks/:deck_id/cards/:id/question' do
+  @round = Round.new
+  @deck = Deck.find(params[:deck_id])
+  @cards = @deck.cards.shuffle
+  @card = @cards.shift
+  @question = @card.question
+  erb :'decks/card_question'
+end
+
+get '/decks/:deck_id/cards/:id/answer' do
+  erb :'decks/card_answer'
+end
+
 post '/' do
 
 end
