c45cacdfb8ae6e507019f7efad8a5f991abc887b
Nick Perri <nperri6@gmail.com>
2015-10-24 19:03:10 -0400

    add post to rounds/:id

--- app/controllers/round_controller.rb
+++ app/controllers/round_controller.rb
@@ -1,9 +1,9 @@
 get '/rounds/:id' do
 
   # @guesses = Guess.where(round_id: session[:round_id]).where(is_correct: false)
-  round = Round.find(session[:round_id])
-  @guesses = round.guesses.where(is_correct: false)
-  @deck = round.deck
+  @round = Round.find(session[:round_id])
+  @guesses = @round.guesses.where(is_correct: false)
+  @deck = @round.deck
   @cards = []
   @guesses.each do |guess|
       @cards << Card.where(id: guess[:card_id])
@@ -16,4 +16,10 @@ get '/rounds/:id' do
     erb :'rounds/show'
   end
 
+end
+
+post '/rounds/:id' do
+  @guess = Guess.where(round_id: params[:id]).where(card_id: params[:card])
+  @guess.first[:is_correct] = params[:is_correct]
+  redirect "/rounds/#{params[:id]}"
 end
