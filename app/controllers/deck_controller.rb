87780c8a8a2f809025202a934183c962c73082a6
Rebecca Kleinberg <email": null>
2015-10-24 20:07:18 -0400

    unregistered user can play game, and message will be shown to unregistered users

--- app/controllers/deck_controller.rb
+++ app/controllers/deck_controller.rb
@@ -7,6 +7,10 @@ get '/decks/:deck_id' do
   if logged_in?
     @round = Round.new(user_id: session[:user_id], deck_id: params[:deck_id])
     @round.save
+  else
+    @round = Round.new(deck_id: params[:deck_id])
+    @round.save
+  end
 
     @deck = @round.deck
     @cards = @deck.cards.to_a
@@ -14,24 +18,6 @@ get '/decks/:deck_id' do
     @cards.each do |card|
       Guess.create!(round_id: @round.id, card_id: card.id, is_correct: false, times_guessed: 0)
     end
-
     session[:round_id] = @round.id
     redirect to("/rounds/#{@round.id}")
-  end
-
-
 end
-
-
-# post '/' do
-
-# end
-
-# get '/' do
-
-# end
-
-# get '/' do
-#   erb :''
-# end
-
--- app/controllers/round_controller.rb
+++ app/controllers/round_controller.rb
@@ -1,6 +1,5 @@
 get '/rounds/:id' do
 
-  # @guesses = Guess.where(round_id: session[:round_id]).where(is_correct: false)
   @round = Round.find(session[:round_id])
   @guesses = @round.guesses.where(is_correct: false)
   @deck = @round.deck
@@ -10,7 +9,11 @@ get '/rounds/:id' do
   end
 
   if @cards == []
-    erb :'rounds/stats'
+    if logged_in?
+      erb :'rounds/stats'
+    else
+      redirect '/'
+    end
   else
     @card = @cards.sample.first
     erb :'rounds/show'
@@ -20,11 +23,6 @@ end
 
 post '/rounds/:id' do
   @guess = Guess.where(round_id: params[:id]).where(card_id: params[:card])
-  # if params[:is_correct] == "false"
-  #   @status = false
-  # else
-  #   @status = true
-  # end
   @guess.first[:is_correct] = params[:is_correct]
   @guess.first[:times_guessed] += 1
   @guess.first.save
